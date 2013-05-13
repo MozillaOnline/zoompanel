17579e207a72ed87ecf08f59df415ffe5bf7977d
JiaoFeng <fjiao@mozilla.com>
2013-05-13 10:26:52 +0800

    change version for update to AMO

--- defaults/preferences/zoompanel.js
+++ defaults/preferences/zoompanel.js
@@ -4,4 +4,7 @@
 
 pref("extensions.zoompanel@mozillaonline.com.description", "chrome://zoompanel/locale/zoompanel.properties");
 pref("extensions.zoompanel@mozillaonline.com.firstrun", true);
+
+// We think following Settings is best for users
+//
 pref("toolkit.zoomManager.zoomValues", ".5,.75,1,1.25,1.5,2,3");
